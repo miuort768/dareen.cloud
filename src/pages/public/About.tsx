58f4f567eb088ad17ddae12fbb485a395afb64c9
dareen-cloud <dareen.cloud@example.com>
2026-01-21 21:35:12 +0200

    Change mobile navbar to relative positioning and remove page top padding on mobile

--- src/pages/public/About.tsx
+++ src/pages/public/About.tsx
@@ -14,7 +14,7 @@ export const About = () => {
             <PublicNavbar />
 
             {/* Hero Section */}
-            <section className="relative pt-24 pb-16 md:pt-48 md:pb-24 overflow-hidden bg-[#FDFCF8]">
+            <section className="relative pt-4 pb-16 md:pt-48 md:pb-24 overflow-hidden bg-[#FDFCF8]">
                 {/* Creative Background Elements */}
                 <div className="absolute top-0 right-0 w-96 h-96 bg-gold/5 rounded-full blur-[100px] -translate-y-1/2 translate-x-1/2"></div>
                 <div className="absolute bottom-0 left-0 w-64 h-64 bg-blue-600/5 rounded-full blur-[80px] translate-y-1/2 -translate-x-1/2"></div>
