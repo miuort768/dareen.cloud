2d1432ce708975a65b3497622546e301b6deb09b
dareen-cloud <dareen.cloud@example.com>
2026-06-04 07:00:37 +0200

    إرجاع محاذاة الفقرات إلى justified (ضبط الطرفين)

--- src/pages/public/BlogPost.tsx
+++ src/pages/public/BlogPost.tsx
@@ -174,7 +174,7 @@ export const BlogPost = () => {
                 {/* Article Content */}
                 <article className="container mx-auto px-4 max-w-3xl">
                     <div 
-                        className="prose sm:prose-lg dark:prose-invert prose-headings:font-heading prose-headings:font-black prose-a:text-red-600 prose-img:shadow-xl max-w-none prose-p:text-center text-[#7F09C8] dark:text-[#179013]"
+                        className="prose sm:prose-lg dark:prose-invert prose-headings:font-heading prose-headings:font-black prose-a:text-red-600 prose-img:shadow-xl max-w-none prose-p:text-justify text-[#7F09C8] dark:text-[#179013]"
                         dangerouslySetInnerHTML={{ __html: sanitizeHTML(processContent(contentParts.first)) }}
                     />
                     
@@ -202,7 +202,7 @@ export const BlogPost = () => {
                     
                     {contentParts.rest && (
                         <div 
-                            className="prose sm:prose-lg dark:prose-invert prose-headings:font-heading prose-headings:font-black prose-a:text-red-600 prose-img:shadow-xl max-w-none mb-0 prose-p:text-center"
+                            className="prose sm:prose-lg dark:prose-invert prose-headings:font-heading prose-headings:font-black prose-a:text-red-600 prose-img:shadow-xl max-w-none mb-0 prose-p:text-justify"
                             dangerouslySetInnerHTML={{ __html: sanitizeHTML(processContent(contentParts.rest)) }}
                         />
                     )}
