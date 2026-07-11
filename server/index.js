bd41aa5bac72952beb9ca66d8505f5b011139156
dareen-cloud <dareen.cloud@example.com>
2026-07-11 22:45:21 +0200

    fix: add Google Analytics domains to CSP script-src to unblock gtag.js

--- server/index.js
+++ server/index.js
@@ -112,7 +112,7 @@ app.use(helmet({
         directives: {
             ...helmet.contentSecurityPolicy.getDefaultDirectives(),
             "img-src": ["'self'", "data:", "https:", "http:"],
-            "script-src": ["'self'", "'unsafe-inline'"],
+            "script-src": ["'self'", "'unsafe-inline'", "https://www.googletagmanager.com", "https://www.google-analytics.com"],
             "connect-src": ["'self'", "https:", "http:", "ws:", "wss:"]
         }
     },
